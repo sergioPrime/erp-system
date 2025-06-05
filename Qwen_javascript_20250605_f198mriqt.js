4e5507d9b44aeb0d2aa499aaf8002fd3a805ccbf
sergioPrime <certificados@primegestor.com.br>
2025-06-05 15:16:52 -0400

    Update Qwen_javascript_20250605_f198mriqt.js

--- Qwen_javascript_20250605_f198mriqt.js
+++ Qwen_javascript_20250605_f198mriqt.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const { listarEmpresas, criarEmpresa } = require('../controllers/empresaController');
 
-const router = express.Router();
-
-router.get('/', listarEmpresas);
-router.post('/', criarEmpresa);
-
-module.exports = router;
